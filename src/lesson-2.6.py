18ab992d679ce9123ccdaabb1af06fa77bdf33fb
Sean Poynter <sean.poynter@gmail.com>
2017-07-23 02:21:12 -0500

    Add lesson 2.6 problems.

---
+++ src/lesson-2.6.py
@@ -0,0 +1,17 @@
+import vector
+
+# 1
+a = vector.Vector([-0.221, 7.437])
+print("#1 magnitude: {}".format(a.magnitude()))
+
+# 2
+b = vector.Vector([8.813, -1.331, -6.247])
+print("#2 magnitude: {}".format(b.magnitude()))
+
+# 3
+c = vector.Vector([5.581, -2.136])
+print("#3 direction: {}".format(c.direction()))
+
+# 4
+d = vector.Vector([1.996, 3.108, -4.554])
+print("#4 direction: {}".format(d.direction()))
