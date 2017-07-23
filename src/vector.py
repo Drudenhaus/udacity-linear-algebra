5e5e23ef5e99dfc6f701bb79d26251dce7ec191d
Sean Poynter <sean.poynter@gmail.com>
2017-07-23 01:32:58 -0500

    Implement addition, subtraction, multiplication, and length functionality.

--- src/vector.py
+++ src/vector.py
@@ -1,7 +1,5 @@
 """
 Object class representation of an vector.
-
-This code is provided with the course.
 """
 
 
@@ -19,8 +17,30 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
+    def __len__(self):
+        return self.dimension
+
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def __add__(self, other):
+        if len(self) == len(other) and isinstance(other, Vector):
+            return Vector([a + b for a, b in zip(self.coordinates, other.coordinates)])
+        else:
+            raise ValueError("Both arguments must be Vector objects of the same length")
+
+    def __sub__(self, other):
+        if len(self) == len(other) and isinstance(other, Vector):
+            return Vector([a - b for a, b in zip(self.coordinates, other.coordinates)])
+        else:
+            raise ValueError("Both arguments must be Vector objects of the same length")
+
+    def __mul__(self, scalar):
+        try:
+            float(scalar)
+            return Vector([coordinate * scalar for coordinate in self.coordinates])
+        except ValueError:
+            raise ValueError("Scalar must be a real number")
